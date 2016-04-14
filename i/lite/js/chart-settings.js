2c96b52c2b7a2515aca46eb67869f3f42a8f75d0
y.vlasenko <vlasenkoj@gmail.com>
2016-04-14 16:21:55 +0300

    first commit

---
+++ i/lite/js/chart-settings.js
@@ -0,0 +1,547 @@
+      window.onload = function() {
+        chart1();
+        chart2();
+        chart3();
+        chart4();
+        chart5();
+        chart6();
+        chart7();
+        chart8();
+        chart9();
+        chart10();
+        chart11();
+        chart12();
+        chart13();
+        chart14();
+        chart15();
+        chart16();
+        chart17();
+        chart18();
+        chart19();
+        chart20();
+      };
+
+      function chart1() {
+        var ctx = $("#small-chart-1").get(0).getContext("2d");
+
+        var data = {
+          labels: ["Позавчера", "Вчера", "Сегодня"],
+          datasets: [
+            {
+              strokeColor: "#505050",
+              fillColor: "#d8d8d8",
+              data: [49, 47, 81]
+            }
+          ]
+        }
+        var options = {
+          animation: false,
+          showScale: false,
+          responsive: true,
+          showTooltips: false,
+          pointDot : false,
+          scaleBackdropPaddingY : 0,
+          scaleBackdropPaddingX : 0
+        }
+
+        var myNewChart = new Chart(ctx).Line(data, options);
+      }
+
+      function chart2() {
+        var ctx = $("#small-chart-2").get(0).getContext("2d");
+
+        var data = {
+          labels: ["Позавчера", "Вчера", "Сегодня"],
+          datasets: [
+            {
+              strokeColor: "#505050",
+              fillColor: "#d8d8d8",
+              data: [80, 80, 80]
+            }
+          ]
+        }
+        var options = {
+          animation: false,
+          showScale: false,
+          responsive: true,
+          showTooltips: false,
+          pointDot : false,
+          scaleBackdropPaddingY : 0,
+          scaleBackdropPaddingX : 0
+        }
+
+        var myNewChart = new Chart(ctx).Line(data, options);
+      }
+
+      function chart3() {
+        var ctx = $("#small-chart-3").get(0).getContext("2d");
+
+        var data = {
+          labels: ["Позавчера", "Вчера", "Сегодня"],
+          datasets: [
+            {
+              strokeColor: "#505050",
+              fillColor: "#d8d8d8",
+              data: [5, 40, 80]
+            }
+          ]
+        }
+        var options = {
+          animation: false,
+          showScale: false,
+          responsive: true,
+          showTooltips: false,
+          pointDot : false,
+          scaleBackdropPaddingY : 0,
+          scaleBackdropPaddingX : 0
+        }
+
+        var myNewChart = new Chart(ctx).Line(data, options);
+      }
+
+      function chart4() {
+        var ctx = $("#small-chart-4").get(0).getContext("2d");
+
+        var data = {
+          labels: ["Позавчера", "Вчера", "Сегодня"],
+          datasets: [
+            {
+              strokeColor: "#505050",
+              fillColor: "#d8d8d8",
+              data: [80, 100, 40]
+            }
+          ]
+        }
+        var options = {
+          animation: false,
+          showScale: false,
+          responsive: true,
+          showTooltips: false,
+          pointDot : false,
+          scaleBackdropPaddingY : 0,
+          scaleBackdropPaddingX : 0
+        }
+
+        var myNewChart = new Chart(ctx).Line(data, options);
+      }
+
+      function chart5() {
+        var ctx = $("#small-chart-5").get(0).getContext("2d");
+
+        var data = {
+          labels: ["Позавчера", "Вчера", "Сегодня"],
+          datasets: [
+            {
+              strokeColor: "#505050",
+              fillColor: "#d8d8d8",
+              data: [1, 2, 3]
+            }
+          ]
+        }
+        var options = {
+          animation: false,
+          showScale: false,
+          responsive: true,
+          showTooltips: false,
+          pointDot : false,
+          scaleBackdropPaddingY : 0,
+          scaleBackdropPaddingX : 0
+        }
+
+        var myNewChart = new Chart(ctx).Line(data, options);
+      }
+
+      function chart6() {
+        var ctx = $("#small-chart-6").get(0).getContext("2d");
+
+        var data = {
+          labels: ["Позавчера", "Вчера", "Сегодня"],
+          datasets: [
+            {
+              strokeColor: "#505050",
+              fillColor: "#d8d8d8",
+              data: [60, 40, 80]
+            }
+          ]
+        }
+        var options = {
+          animation: false,
+          showScale: false,
+          responsive: true,
+          showTooltips: false,
+          pointDot : false,
+          scaleBackdropPaddingY : 0,
+          scaleBackdropPaddingX : 0
+        }
+
+        var myNewChart = new Chart(ctx).Line(data, options);
+      }
+
+      function chart7() {
+        var ctx = $("#small-chart-7").get(0).getContext("2d");
+
+        var data = {
+          labels: ["Позавчера", "Вчера", "Сегодня"],
+          datasets: [
+            {
+              strokeColor: "#505050",
+              fillColor: "#d8d8d8",
+              data: [80, 80, 10]
+            }
+          ]
+        }
+        var options = {
+          animation: false,
+          showScale: false,
+          responsive: true,
+          showTooltips: false,
+          pointDot : false,
+          scaleBackdropPaddingY : 0,
+          scaleBackdropPaddingX : 0
+        }
+
+        var myNewChart = new Chart(ctx).Line(data, options);
+      }
+
+      function chart8() {
+        var ctx = $("#small-chart-8").get(0).getContext("2d");
+
+        var data = {
+          labels: ["Позавчера", "Вчера", "Сегодня"],
+          datasets: [
+            {
+              strokeColor: "#505050",
+              fillColor: "#d8d8d8",
+              data: [80, 80, 100]
+            }
+          ]
+        }
+        var options = {
+          animation: false,
+          showScale: false,
+          responsive: true,
+          showTooltips: false,
+          pointDot : false,
+          scaleBackdropPaddingY : 0,
+          scaleBackdropPaddingX : 0
+        }
+
+        var myNewChart = new Chart(ctx).Line(data, options);
+      }
+
+      function chart9() {
+        var ctx = $("#small-chart-9").get(0).getContext("2d");
+
+        var data = {
+          labels: ["Позавчера", "Вчера", "Сегодня"],
+          datasets: [
+            {
+              strokeColor: "#505050",
+              fillColor: "#d8d8d8",
+              data: [70, 79, 80]
+            }
+          ]
+        }
+        var options = {
+          animation: false,
+          showScale: false,
+          responsive: true,
+          showTooltips: false,
+          pointDot : false,
+          scaleBackdropPaddingY : 0,
+          scaleBackdropPaddingX : 0
+        }
+
+        var myNewChart = new Chart(ctx).Line(data, options);
+      }
+
+      function chart10() {
+        var ctx = $("#small-chart-10").get(0).getContext("2d");
+
+        var data = {
+          labels: ["Позавчера", "Вчера", "Сегодня"],
+          datasets: [
+            {
+              strokeColor: "#505050",
+              fillColor: "#d8d8d8",
+              data: [76, 81, 76]
+            }
+          ]
+        }
+        var options = {
+          animation: false,
+          showScale: false,
+          responsive: true,
+          showTooltips: false,
+          pointDot : false,
+          scaleBackdropPaddingY : 0,
+          scaleBackdropPaddingX : 0
+        }
+
+        var myNewChart = new Chart(ctx).Line(data, options);
+      }
+
+      function chart11() {
+        var ctx = $("#small-chart-11").get(0).getContext("2d");
+
+        var data = {
+          labels: ["Позавчера", "Вчера", "Сегодня"],
+          datasets: [
+            {
+              strokeColor: "#505050",
+              fillColor: "#d8d8d8",
+              data: [49, 47, 81]
+            }
+          ]
+        }
+        var options = {
+          animation: false,
+          showScale: false,
+          responsive: true,
+          showTooltips: false,
+          pointDot : false,
+          scaleBackdropPaddingY : 0,
+          scaleBackdropPaddingX : 0
+        }
+
+        var myNewChart = new Chart(ctx).Line(data, options);
+      }
+
+      function chart12() {
+        var ctx = $("#small-chart-12").get(0).getContext("2d");
+
+        var data = {
+          labels: ["Позавчера", "Вчера", "Сегодня"],
+          datasets: [
+            {
+              strokeColor: "#505050",
+              fillColor: "#d8d8d8",
+              data: [80, 80, 80]
+            }
+          ]
+        }
+        var options = {
+          animation: false,
+          showScale: false,
+          responsive: true,
+          showTooltips: false,
+          pointDot : false,
+          scaleBackdropPaddingY : 0,
+          scaleBackdropPaddingX : 0
+        }
+
+        var myNewChart = new Chart(ctx).Line(data, options);
+      }
+
+      function chart13() {
+        var ctx = $("#small-chart-13").get(0).getContext("2d");
+
+        var data = {
+          labels: ["Позавчера", "Вчера", "Сегодня"],
+          datasets: [
+            {
+              strokeColor: "#505050",
+              fillColor: "#d8d8d8",
+              data: [5, 40, 80]
+            }
+          ]
+        }
+        var options = {
+          animation: false,
+          showScale: false,
+          responsive: true,
+          showTooltips: false,
+          pointDot : false,
+          scaleBackdropPaddingY : 0,
+          scaleBackdropPaddingX : 0
+        }
+
+        var myNewChart = new Chart(ctx).Line(data, options);
+      }
+
+      function chart14() {
+        var ctx = $("#small-chart-14").get(0).getContext("2d");
+
+        var data = {
+          labels: ["Позавчера", "Вчера", "Сегодня"],
+          datasets: [
+            {
+              strokeColor: "#505050",
+              fillColor: "#d8d8d8",
+              data: [80, 100, 40]
+            }
+          ]
+        }
+        var options = {
+          animation: false,
+          showScale: false,
+          responsive: true,
+          showTooltips: false,
+          pointDot : false,
+          scaleBackdropPaddingY : 0,
+          scaleBackdropPaddingX : 0
+        }
+
+        var myNewChart = new Chart(ctx).Line(data, options);
+      }
+
+      function chart15() {
+        var ctx = $("#small-chart-15").get(0).getContext("2d");
+
+        var data = {
+          labels: ["Позавчера", "Вчера", "Сегодня"],
+          datasets: [
+            {
+              strokeColor: "#505050",
+              fillColor: "#d8d8d8",
+              data: [1, 2, 3]
+            }
+          ]
+        }
+        var options = {
+          animation: false,
+          showScale: false,
+          responsive: true,
+          showTooltips: false,
+          pointDot : false,
+          scaleBackdropPaddingY : 0,
+          scaleBackdropPaddingX : 0
+        }
+
+        var myNewChart = new Chart(ctx).Line(data, options);
+      }
+
+      function chart16() {
+        var ctx = $("#small-chart-16").get(0).getContext("2d");
+
+        var data = {
+          labels: ["Позавчера", "Вчера", "Сегодня"],
+          datasets: [
+            {
+              strokeColor: "#505050",
+              fillColor: "#d8d8d8",
+              data: [60, 40, 80]
+            }
+          ]
+        }
+        var options = {
+          animation: false,
+          showScale: false,
+          responsive: true,
+          showTooltips: false,
+          pointDot : false,
+          scaleBackdropPaddingY : 0,
+          scaleBackdropPaddingX : 0
+        }
+
+        var myNewChart = new Chart(ctx).Line(data, options);
+      }
+
+      function chart17() {
+        var ctx = $("#small-chart-17").get(0).getContext("2d");
+
+        var data = {
+          labels: ["Позавчера", "Вчера", "Сегодня"],
+          datasets: [
+            {
+              strokeColor: "#505050",
+              fillColor: "#d8d8d8",
+              data: [80, 80, 10]
+            }
+          ]
+        }
+        var options = {
+          animation: false,
+          showScale: false,
+          responsive: true,
+          showTooltips: false,
+          pointDot : false,
+          scaleBackdropPaddingY : 0,
+          scaleBackdropPaddingX : 0
+        }
+
+        var myNewChart = new Chart(ctx).Line(data, options);
+      }
+
+      function chart18() {
+        var ctx = $("#small-chart-18").get(0).getContext("2d");
+
+        var data = {
+          labels: ["Позавчера", "Вчера", "Сегодня"],
+          datasets: [
+            {
+              strokeColor: "#505050",
+              fillColor: "#d8d8d8",
+              data: [80, 80, 100]
+            }
+          ]
+        }
+        var options = {
+          animation: false,
+          showScale: false,
+          responsive: true,
+          showTooltips: false,
+          pointDot : false,
+          scaleBackdropPaddingY : 0,
+          scaleBackdropPaddingX : 0
+        }
+
+        var myNewChart = new Chart(ctx).Line(data, options);
+      }
+
+      function chart19() {
+        var ctx = $("#small-chart-19").get(0).getContext("2d");
+
+        var data = {
+          labels: ["Позавчера", "Вчера", "Сегодня"],
+          datasets: [
+            {
+              strokeColor: "#505050",
+              fillColor: "#d8d8d8",
+              data: [70, 79, 80]
+            }
+          ]
+        }
+        var options = {
+          animation: false,
+          showScale: false,
+          responsive: true,
+          showTooltips: false,
+          pointDot : false,
+          scaleBackdropPaddingY : 0,
+          scaleBackdropPaddingX : 0
+        }
+
+        var myNewChart = new Chart(ctx).Line(data, options);
+      }
+
+      function chart20() {
+        var ctx = $("#small-chart-20").get(0).getContext("2d");
+
+        var data = {
+          labels: ["Позавчера", "Вчера", "Сегодня"],
+          datasets: [
+            {
+              strokeColor: "#505050",
+              fillColor: "#d8d8d8",
+              data: [76, 81, 76]
+            }
+          ]
+        }
+        var options = {
+          animation: false,
+          showScale: false,
+          responsive: true,
+          showTooltips: false,
+          pointDot : false,
+          scaleBackdropPaddingY : 0,
+          scaleBackdropPaddingX : 0
+        }
+
+        var myNewChart = new Chart(ctx).Line(data, options);
+      }
+
+
+
+
+
