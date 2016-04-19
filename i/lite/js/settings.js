f4b3ddd077d7a32e2df067d06930cbe2f35bae5a
y.vlasenko <vlasenkoj@gmail.com>
2016-04-19 20:03:38 +0300

    first commit

--- i/lite/js/settings.js
+++ i/lite/js/settings.js
@@ -48,27 +48,31 @@ $(function () {
 });
 
 /* Social graph */
-var donut = new Morris.Donut({
-    element: 'social-chart',
-    resize: true,
-    colors: ["#3b5998", "#00aced", "#dd4b39", "#45668e", "#ed812b"],
-    data: [
-        {label: "Facebook", value: 8304},
-        {label: "Twitter", value: 2304},
-        {label: "Google", value: 1902},
-        {label: "Вконтакте", value: 3094},
-        {label: "Одноклассники", value: 2839}
-    ],
-    hideHover: 'auto',
-    labelColor: '#333333',
+
+$(function () {
+    var f = $(document).find("#social-chart");
+    f.length > 0 && Morris.Donut({
+        element: f,
+        resize: true,
+        colors: ["#3b5998", "#00aced", "#dd4b39", "#45668e", "#ed812b"],
+        data: [
+            {label: "Facebook", value: 8304},
+            {label: "Twitter", value: 2304},
+            {label: "Google", value: 1902},
+            {label: "Вконтакте", value: 3094},
+            {label: "Одноклассники", value: 2839}
+        ],
+        hideHover: 'auto',
+        labelColor: '#333333',
+    });
 });
 
 
 /* Small graph */
 
-$('#small-chart-1, #small-chart-2, #small-chart-3, #small-chart-4, #small-chart-5, #small-chart-6, #small-chart-7, #small-chart-8, #small-chart-9, #small-chart-10, #small-chart-11, #small-chart-12, #small-chart-13, #small-chart-14, #small-chart-15, #small-chart-16, #small-chart-17, #small-chart-18, #small-chart-19, #small-chart-20').sparkline('html',
+$('#small-chart-1, #small-chart-2, #small-chart-3, #small-chart-4, #small-chart-5, #small-chart-6, #small-chart-7, #small-chart-8, #small-chart-9, #small-chart-10, #small-chart-11, #small-chart-12, #small-chart-13, #small-chart-14, #small-chart-15, #small-chart-16, #small-chart-17, #small-chart-18, #small-chart-19, #small-chart-20, #small-chart-21, #small-chart-22, #small-chart-23, #small-chart-24, #small-chart-25, #small-chart-26, #small-chart-27, #small-chart-28, #small-chart-29, #small-chart-30').sparkline('html',
     {   height: '40',
-        width: '100%',
+        width: '74',
         lineColor: '#505050',
         fillColor: '#d8d8d8',
         highlightLineColor: '#337ab7',
@@ -78,4 +82,70 @@ $('#small-chart-1, #small-chart-2, #small-chart-3, #small-chart-4, #small-chart-
         spotColor: '#505050',
         spotRadius: 2
     });
+
+
+/* Datepicker */
+
+$(function () {
+
+    $('#dateRange').daterangepicker({
+        "ranges": {
+            "Сегодня": [
+                "2016-04-19",
+                "2016-04-19"
+            ],
+            "Вчера": [
+                "",
+                "2016-04-18"
+            ],
+            "За неделю": [
+                "2016-04-13",
+                "2016-04-19"
+            ],
+            "За месяц": [
+                "2016-03-21",
+                "2016-04-19"
+            ],
+            "В этом месяце": [
+                "2016-03-31",
+                "2016-04-30"
+            ]
+        },
+        "locale": {
+            "separator": " - ",
+            "applyLabel": "Применить",
+            "cancelLabel": "Отмена",
+            "fromLabel": "С",
+            "toLabel": "До",
+            "customRangeLabel": "Указать",
+            "daysOfWeek": [
+                "Вс",
+                "Пн",
+                "Вт",
+                "Ср",
+                "Чт",
+                "Пт",
+                "Сб"
+            ],
+            "monthNames": [
+                "Январь",
+                "Февраль",
+                "Март",
+                "Апрель",
+                "Май",
+                "Июнь",
+                "Июль",
+                "Август",
+                "Сентябрь",
+                "Октябрь",
+                "Ноябрь",
+                "Декабрь"
+            ],
+            "firstDay": 1
+        },
+        "startDate": "04/13/2016",
+        "endDate": "04/19/2016"
+    })
+
+});
 
