90c417ad9c8bd76d2bcc2eb16bd761e03f7bdb15
y.vlasenko <vlasenkoj@gmail.com>
2016-04-19 15:15:03 +0300

    first commit

--- i/lite/js/settings.js
+++ i/lite/js/settings.js
@@ -1,3 +1,4 @@
+/* Dropdown */
 /*Speedometer views*/
 $(function() {
     $(".sp").knob({
@@ -46,10 +47,27 @@ $(function () {
     });
 });
 
+/* Social graph */
+var donut = new Morris.Donut({
+    element: 'social-chart',
+    resize: true,
+    colors: ["#3b5998", "#00aced", "#dd4b39", "#45668e", "#ed812b"],
+    data: [
+        {label: "Facebook", value: 8304},
+        {label: "Twitter", value: 2304},
+        {label: "Google", value: 1902},
+        {label: "Вконтакте", value: 3094},
+        {label: "Одноклассники", value: 2839}
+    ],
+    hideHover: 'auto',
+    labelColor: '#333333',
+});
+
+
 /* Small graph */
 
 $('#small-chart-1, #small-chart-2, #small-chart-3, #small-chart-4, #small-chart-5, #small-chart-6, #small-chart-7, #small-chart-8, #small-chart-9, #small-chart-10, #small-chart-11, #small-chart-12, #small-chart-13, #small-chart-14, #small-chart-15, #small-chart-16, #small-chart-17, #small-chart-18, #small-chart-19, #small-chart-20').sparkline('html',
-    {   height: '50',
+    {   height: '40',
         width: '100%',
         lineColor: '#505050',
         fillColor: '#d8d8d8',
